679418aa00d1dfe6b2bad57c3cc41f7d2a1541d9
Barbara Azevedo <barbara.azevedo@sap.com>
2026-05-27 11:49:08 -0300

    create method for formatDate to utils file

--- utils.js
+++ utils.js
@@ -1,3 +1,13 @@
 function capitalizeText(text) {
     return text.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
 }
+
+function formatDate(date = new Date()) {
+    return new Intl.DateTimeFormat("en-US", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit"
+    }).format(date);
+}
