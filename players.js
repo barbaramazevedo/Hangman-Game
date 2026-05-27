6e65735b4e60c2629860d5338dd3a04dff8f9f7a
Barbara Azevedo <barbara.azevedo@sap.com>
2026-05-27 10:31:57 -0300

    implement updateScore method

--- players.js
+++ players.js
@@ -15,10 +15,21 @@ function searchPlayer(name) {
     return players.find(j => j.name === name);
 }
 
-function atualizarPontuacao(nome, venceu) {
-    
+function updateScore(name, won) {
+    const players = loadPlayers();
+    const player = players.find(p => p.name === name);
+    if (!player) return false;
+
+    player.matches++;
+    if (won) {
+        player.wins++;
+    } else {
+        player.losses++;
+    }
+    player.score = player.wins * 10 - player.losses * 5;
+
+    savePlayers(players);
+    return true;
 }
 
 
-function obterJogadores() {
-}
